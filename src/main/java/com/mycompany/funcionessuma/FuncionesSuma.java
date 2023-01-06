76575c8a88e92b99d7759e0c34a1f55f6e53363c
alexn <alexn@LAPTOP-C86TGOOS>
2023-01-06 11:27:57 -0500

    Funsion Suma tres numeros

---
+++ src/main/java/com/mycompany/funcionessuma/FuncionesSuma.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.funcionessuma;
+
+/**
+ *
+ * @author Wilmer Alexander Varon Rodriguez 
+ */
+public class FuncionesSuma {
+    
+    public static int suma(int a,int b, int c){
+    
+        return a+b+c;
+    }
+
+    public static void main(String[] args) {
+        
+            
+        System.out.println (suma(5,10,15));
+    }
+}
